0604b80b2ea2f23898ade5c45aa9ff671cebdbc0
Anthony Hauger <ahauger@dontdrivedirty.com>
2021-09-21 13:39:43 -0700

    feat: removed healthcheck handler

--- main.go
+++ main.go
@@ -2,14 +2,8 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println("Lorem impsum other latin stuff")
-
-	r := gin.New()
-	r.GET("/healthcheck", func(c *gin.Context) { c.JSON(http.StatusOK, map[string]bool{"ok": true}) })
 }
